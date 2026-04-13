94fe8f067adabfadd01c8ca2ceb24295347f446e
martijn-van-gameren <martijn.gameren@galileo-itservices.nl>
2026-04-13 11:47:01 +0200

    navbar

---
+++ bff-frontend/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header() {
+    return (
+        <div className='nav-container'>
+            <p className='nav-item'>Posts</p>
+            <p className='nav-item'>About</p>
+            <p className='nav-item'>News</p>
+        </div>
+    )
+}
+
+export default Header
